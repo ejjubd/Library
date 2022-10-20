1fa3387d869939ef3598c2205cd5d6b072c4dae5
ejjubd <ejjubdemir@hotmail.com>
2022-10-20 14:25:42 +0300

    initial add book in table finished

--- script.js
+++ script.js
@@ -1,18 +1,78 @@
 const closeButton = document.querySelector("#close-button")
-const addBookButton = document.querySelector("#table-footer-icons-book")
-
+const addNewBookButton = document.querySelector("#table-footer-icons-book")
+const addBookSubmit = document.querySelector('#add-book-button')
 closeButton.addEventListener('click', function(){
     document.getElementById("add-book-window").style.display = 'none'
 
 })
 
-addBookButton.addEventListener('click',function(){
+addNewBookButton.addEventListener('click',function(){
     document.getElementById("add-book-window").style.display = 'block' 
 })
 
 
+addBookSubmit.addEventListener('click',function(){
+    
+    const getTable= document.getElementById('table');
+    createNewRow = document.createElement('tr')
+    getTable.appendChild(createNewRow)
+
+th1 = document.createElement('th');
+th1.innerHTML = '<input type="checkbox" name="" id="">';
+
+th2 = document.createElement('th');
+th2.innerHTML = 'Title';
+
+th3 = document.createElement('th');
+th3.innerHTML = 'Title';
+
+th4 = document.createElement('th');
+th4.innerHTML = 'Title';
+
+th5 = document.createElement('th');
+th5.innerHTML = 'Title';
+
+th6 = document.createElement('th');
+th6.innerHTML = 'Title';
+
+th7 = document.createElement('th');
+th7.innerHTML = '<a id="status" href="">Read</a>';
+
+th8 = document.createElement('th');
+th8.innerHTML = '<a class="table-icons" href=""><img src="./images/delete.png" alt=""></a><a class="table-icons" href=""><img src="./images/edit.png" alt=""></a>';
 
 
+
+createNewRow.appendChild(th1);
+createNewRow.appendChild(th2);
+createNewRow.appendChild(th3);
+createNewRow.appendChild(th4);
+createNewRow.appendChild(th5);
+createNewRow.appendChild(th6);
+createNewRow.appendChild(th7);
+createNewRow.appendChild(th8);
+   
+document.getElementById("add-book-window").style.display = 'none' 
+  
+
+    
+ })
+
+
+function Book (title, author, pages, hasread){
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.hasRead = hasread
+    this.info = function () {
+        console.log(title, author, pages, hasread)
+    }
+}
+
+
+const Book1 = new Book ('Dostojevski', 'Rat i mir', 986, 'not read yet')
+
+Book1.info();
 
 
 
@@ -63,7 +123,7 @@ function Book (title, author, pages, hasread){
 }
 
 
-const Book1 = new Book ('Dostojevski', 'Rat i mir', 986, 'not read yet')
+
 
 Book1.info();
 
